ff069eac98769470320fa00760885782194f4b57
Ayhan Unlu <103220953+ayhan-unlu@users.noreply.github.com>
2025-09-03 21:53:08 +0300

    Initial commit (Java Spring Boot Homework-Patika.dev TwoWay Server Client Web Socket Spring Boot)

---
+++ src/test/java/com/ayhanunlu/WebSocketSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ayhanunlu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSocketSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
