ed5719464c200847de509026505ab182a0a8c0e7
Ayhan Unlu <103220953+ayhan-unlu@users.noreply.github.com>
2025-09-03 22:11:33 +0300

    pom.xml no change WebSocketSpringBootApplication no change WebSocketSpringBootApplicationTests no change

--- src/main/java/com/ayhanunlu/WebSocketSpringBootApplication.java
+++ src/main/java/com/ayhanunlu/WebSocketSpringBootApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WebSocketSpringBootApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WebSocketSpringBootApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WebSocketSpringBootApplication.class, args);
+    }
 
 }
