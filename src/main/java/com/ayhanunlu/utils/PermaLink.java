eb5f6e2c471d98e23c24ed9ab612c286f6687983
Ayhan Unlu <103220953+ayhan-unlu@users.noreply.github.com>
2025-09-03 22:09:32 +0300

    PermaLink

---
+++ src/main/java/com/ayhanunlu/utils/PermaLink.java
@@ -0,0 +1,9 @@
+package com.ayhanunlu.utils;
+
+import org.springframework.boot.autoconfigure.info.ProjectInfoProperties;
+
+public class PermaLink {
+
+//    GitHub: https://github.com/ayhan-unlu/WebSocketSpringBoot
+
+}
