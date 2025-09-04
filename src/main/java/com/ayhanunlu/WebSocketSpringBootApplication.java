b722ee65e2ffc627f30fb84a0ea1ace03be9c549
Ayhan Unlu <103220953+ayhan-unlu@users.noreply.github.com>
2025-09-04 12:41:16 +0300

    WebSocketSpringBootApplication (Security excluded)

--- src/main/java/com/ayhanunlu/WebSocketSpringBootApplication.java
+++ src/main/java/com/ayhanunlu/WebSocketSpringBootApplication.java
@@ -2,8 +2,15 @@ package com.ayhanunlu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/*
+// Security Active
 @SpringBootApplication
+*/
+
+//Security Inactive
+@SpringBootApplication(exclude = {
+        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+        org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class})
 public class WebSocketSpringBootApplication {
 
     public static void main(String[] args) {
